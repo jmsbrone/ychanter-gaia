299e2a063d56faec279b569ec0b207bdd420f4b0
jmsbrone <jms.brone@gmail.com>
2022-07-02 21:52:24 +0300

    Removing dynamic entity

--- src/client/modules/auth/types/user-permissions-subject.ts
+++ src/client/modules/auth/types/user-permissions-subject.ts
@@ -1,8 +1,7 @@
 import { Ability } from "@casl/ability";
 import { User } from "client/modules/auth/domains/user";
 import { UserAbility } from "client/modules/auth/values/user-ability";
-import { DynamicEntity } from "client/modules/dynamic-entity/domains/dynamic-entity";
 import { UserAction } from "common/modules/auth/types/user-action";
 
-export type UserPermissionsSubject = "all" | User | DynamicEntity;
+export type UserPermissionsSubject = "all" | User;
 export class UserPermissions extends Ability<[UserAction, UserAbility]> {}
