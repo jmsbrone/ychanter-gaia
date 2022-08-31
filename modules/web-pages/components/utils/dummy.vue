b42cb58d03495860b257839d81d936d1dcdd49f3
jmsbrone <jms.brone@gmail.com>
2022-08-31 21:28:29 +0300

    Removing web-pages module from main application branch

--- modules/web-pages/components/utils/dummy.vue
+++
@@ -1,3 +0,0 @@
-<template lang="pug">
-div.text-red Unsupported component
-</template>
--- modules/web-pages/domains/web-page-tree.ts
+++
@@ -1,6 +0,0 @@
-import { WebPage } from "./web-page";
-
-export class WebPageTree extends WebPage {
-    children: WebPageTree[] = [];
-    expanded: boolean = false;
-}
