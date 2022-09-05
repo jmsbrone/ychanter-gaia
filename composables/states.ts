91525bd717f69d3d81e9b0bc0c95b41144ca8be5
jmsbrone <jms.brone@gmail.com>
2022-09-05 20:44:56 +0300

    Adding base module and futher removing other module functionality

--- composables/states.ts
+++ composables/states.ts
@@ -1,1 +1,28 @@
+import _ from "lodash";
+
 export const useCurrentUser = () => useState("currentUser", () => null);
+
+export function useAdminFooter() {
+    const components = [];
+    let show = false;
+
+    return {
+        addComponent(name: string) {
+            if (!_.has(components, name)) {
+                components.push(name);
+            }
+        },
+        getComponents() {
+            return components;
+        },
+        isVisible() {
+            return show;
+        },
+        hide() {
+            show = false;
+        },
+        show() {
+            show = true;
+        },
+    };
+}
